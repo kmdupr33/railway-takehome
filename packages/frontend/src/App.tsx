85d979ee97f96f5e53118d260eae2061ec26a363
Matt Dupree <mattdupree@mac.lan>
2024-10-31 14:54:41 -0500

    add basic authorizer login ui

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import {
+  AuthorizerProvider,
+  Authorizer,
+  useAuthorizer,
+} from '@authorizerdev/authorizer-react'
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <AuthorizerProvider
+      config={{
+        authorizerURL: import.meta.env.VITE_AUTHORIZER_URL,
+        redirectURL: window.location.origin,
+        clientID: import.meta.env.VITE_AUTHORIZER_CLIENT_ID, // obtain your client id from authorizer dashboard
+      }}
+    >
+      <LoginSignup />
+      <Profile />
+    </AuthorizerProvider>
   )
+}
+
+function LoginSignup() {
+  return <Authorizer/>
+}
+
+function Profile() {
+  const { user } = useAuthorizer()
+ 
+  if (user) {
+    return <div>{user.email}</div>
+  }
+ 
+  return null
 }
 
 export default App
