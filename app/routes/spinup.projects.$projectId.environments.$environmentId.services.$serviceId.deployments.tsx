cc158a67a49d774f2f16bbc25224ff0bf72b143a
Matt Dupree <mattdupree@mac.lan>
2024-11-02 13:32:49 -0500

    remove unneeded graphql-ws lib

--- app/routes/spinup.projects.$projectId.environments.$environmentId.services.$serviceId.deployments.tsx
+++ app/routes/spinup.projects.$projectId.environments.$environmentId.services.$serviceId.deployments.tsx
@@ -1,9 +1,7 @@
 import { json, LoaderFunctionArgs } from "@remix-run/node";
-import { createClient } from "graphql-ws";
-import { useLoaderData, useParams } from "@remix-run/react";
+import { useLoaderData } from "@remix-run/react";
 import { getDeployment } from "~/models/railway.server";
 import { requireUserId } from "~/session.server";
-import { useEffect } from "react";
 
 export const loader = async ({ request, params }: LoaderFunctionArgs) => {
   const userId = await requireUserId(request);
@@ -17,37 +15,8 @@ export const loader = async ({ request, params }: LoaderFunctionArgs) => {
   return json({ deployment });
 };
 
-const client = createClient({
-  url: "wss://backboard.railway.app/graphql",
-});
-
 export default function Deployments() {
   const { deployment } = useLoaderData<typeof loader>();
-  async function subscribe() {
-    console.log("is it happening?!");
-    const subscription = client.iterate<{ deployment: { status: string } }>(
-      /* GraphQL */ {
-        query: `
-      subscription {
-        deployment(id: "${deployment.id}") {
-          status
-        }
-      }
-    `,
-      },
-    );
-    for await (const { data } of subscription) {
-      if (
-        ["SUCCESS", "FAILED", "CRAHSED", "REMOVED", "SLEEPING"].includes(
-          data.deployment.status,
-        )
-      ) {
-      }
-    }
-  }
-  useEffect(() => {
-    subscribe();
-  }, []);
   return (
     <>
     <p>Deployment ID : {deployment.id}</p>
